081816931cfb8fe3e29a8b6d71fe9e5d033b351a
mac duc anh <gumayusi9602@outlook.com>
2024-01-17 17:34:46 +0700

    update 4fun

--- Assets/Scripts/QuetDiem/MatchstickController.cs
+++ Assets/Scripts/QuetDiem/MatchstickController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Scripts.QuetDiem{
@@ -8,13 +10,29 @@ namespace Scripts.QuetDiem{
     {
         [SerializeField] float Zscreen;
         [SerializeField] Vector3 offset;
-        [SerializeField] Transform thisHokage;
         [SerializeField] Transform targetToLook;
         [SerializeField] Transform mouseInWorld;
+        [SerializeField] bool IsTouchMatchBox;
+        [SerializeField] private float currentTime;
+        [SerializeField] float timeMax = 3f;
+
+        [SerializeField] ParticleSystem _effectExcuted;
+        [SerializeField] ParticleSystem _effectStart;
+
+        [SerializeField] Transform currentFire;
+        [SerializeField] Transform startFire;
+
+        private void Start ()
+        {
+            IsTouchMatchBox = false;
+            currentFire = startFire;
+        }
         private void Update()
         {
             Zscreen = Camera.main.WorldToScreenPoint(transform.position).z;
             offset = transform.position - getMouseInWorld();
+
+ 
 
         }
 
@@ -34,10 +52,85 @@ namespace Scripts.QuetDiem{
         private void OnMouseDrag()
         {
             //Debug.Log("halo1");
-            thisHokage.position = getMouseInWorld() + offset;
-            mouseInWorld.position = getMouseInWorld();
-            thisHokage.transform.LookAt(targetToLook);
+
+            //Ray ray = Camera.main.ScreenPointToRay(transform.position);
+            //RaycastHit hit;
+            //Debug.DrawRay(ray.origin,ray.direction, Color.red);
+            //if (Physics.Raycast(ray, out hit) )
+            //{
+            //    float distance = Vector3.Distance(transform.position, hit.point);
+            //    Debug.Log(distance);
+            //    Debug.Log("Hit: " + hit.collider.gameObject.name);
+            //}
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                //can move
+                Move();
+                //cho di chuyen
+
+                //float distance = Vector3.Distance()
+
+                //
+            }
         }
+
+        private void Move()
+        {
+            transform.position = getMouseInWorld() + offset;
+            mouseInWorld.position = getMouseInWorld();
+            transform.LookAt(targetToLook);
+        }
+
+        //private void OnMouseUp()
+        //{
+        //    transform.position = getMouseInWorld() + offset;
+        //}
+
+        //private void OnTriggerStay(Collider other)
+        //{
+        //    if(other.tag == "MatchBox")
+        //    {
+        //        currentTime += Time.deltaTime;
+        //        if(currentTime >= timeMax) {
+        //            SetPosEffectStart();
+        //        }
+        //    }
+        //}
+
+
+        private void SetPosEffectStart()
+        {
+            _effectStart.gameObject.transform.position = startFire.position;
+            _effectExcuted.gameObject.transform.position = startFire.position;
+            StartCoroutine(ExcutedEffect());           
+        }
+
+        IEnumerator ExcutedEffect()
+        {
+            _effectStart.Play();
+            _effectExcuted.Play();
+            yield return new WaitForSeconds(1f);
+            transform.GetComponentInChildren<Animation>().Play();
+            _effectExcuted.gameObject.transform.DOMoveY(0.55f, 4f);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "MatchBox")
+            {
+                Debug.Log("cham nha");
+            }
+        }
+        private void OnCollisionExit(Collision collision)
+        {
+            if(collision.gameObject.tag == "MatchBox")
+            {
+                Debug.Log("toi out");
+            }
+        }
+
+
     }
     
 }
