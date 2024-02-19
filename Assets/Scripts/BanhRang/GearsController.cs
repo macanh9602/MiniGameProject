57a5aca86114dc728c5ef78d68e9e1efa52e02a5
mac duc anh <gumayusi9602@outlook.com>
2024-02-19 17:42:01 +0700

    update

---
+++ Assets/Scripts/BanhRang/GearsController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.BanhRang
+{
+
+    public class GearsController : MonoBehaviour
+    {
+        [SerializeField] Handle handle;
+        [SerializeField] Transform[] arrBigGear;
+        [SerializeField] Transform[] arrMediumGear;
+        [SerializeField] Transform[] arrSmallGear;
+        [SerializeField] Transform[] arrBigGearShadow;
+        [SerializeField] Transform[] arrMediumGearShadow;
+        [SerializeField] Transform[] arrSmallGearShadow;
+
+        [SerializeField] AudioSource audioSource;
+
+        private float sizeBig = 24; //so rang 
+        private float sizeMedium = 18;
+        private float sizeSmall = 12;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (handle.IsMoving)
+            {
+                for (int i = 0; i < arrBigGear.Length; i++)
+                {
+                    arrBigGear[i].Rotate(Vector3.forward, -handle.RotateSpeed);
+                    arrBigGearShadow[i].Rotate(Vector3.forward, -handle.RotateSpeed);
+                }
+                for (int i = 0; i < arrMediumGear.Length; i++)
+                {
+                    float ratio1 = sizeBig / sizeMedium;
+                    arrMediumGear[i].Rotate(Vector3.forward, handle.RotateSpeed * ratio1);
+                    arrMediumGearShadow[i].Rotate(Vector3.forward, handle.RotateSpeed * ratio1);
+                }
+                for (int i = 0; i < arrSmallGear.Length; i++)
+                {
+                    float ratio2 = sizeBig / sizeSmall;
+                    arrSmallGear[i].Rotate(Vector3.forward, -handle.RotateSpeed * ratio2);
+                    arrSmallGearShadow[i].Rotate(Vector3.forward, -handle.RotateSpeed * ratio2);
+                }
+
+                PlaySoundEffect(handle.RotateSpeed);
+
+            }
+        }
+        private float lastPlayTime;
+
+        private void PlaySoundEffect(float rotationSpeed)
+        {
+            float speed = Mathf.Abs(rotationSpeed);
+            if (speed != 0)
+            {
+                float speed1 = Mathf.Max(speed, 0.5f);
+                float playInterval = (1.0f / speed1 )* 0.3f;
+                
+                if (Time.time >= lastPlayTime + playInterval)
+                {
+                    audioSource.PlayOneShot(audioSource.clip);
+                    lastPlayTime = Time.time; 
+                }
+            }
+
+            
+        }
+    }
+
+}
--- Assets/Scripts/BanhRang/Handle.cs
+++ Assets/Scripts/BanhRang/Handle.cs
@@ -1,54 +1,85 @@
-﻿using DG.Tweening;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-namespace Scripts.BanhRang
-{
-
+namespace Scripts.BanhRang{
+    
     public class Handle : MonoBehaviour
     {
-        [SerializeField] Vector3 thisCurrentInWorld;
-
-        [SerializeField] Vector3 posMouseDown;
-        [SerializeField] Vector3 posMouseExcuted;
-        [SerializeField] Transform testA;
-        [SerializeField] Transform testB;
-        private bool isExcuting = false;
-        private bool isInitializing = false;
-        public bool IsExcuting => isExcuting;
-        public bool IsInitializing => isInitializing;
-
-        public Vector3 PosMouseDown { get => posMouseDown; set => posMouseDown = value; }
-        public Vector3 PosMouseExcuted { get => posMouseExcuted; set => posMouseExcuted = value; }
-
-        private void Start()
+        [SerializeField] bool isRotating = false;
+        [SerializeField] Transform dad;
+        private float rotateSpeed;
+        private float projectionX;
+        private float projectionY;
+        private Vector3 lastInput;
+        private Vector3 v_deltaInput;
+        [SerializeField] private float stopTime;
+        private bool isMoving  = false;
+        public bool IsMoving => isMoving;
+        public bool IsRotating => isRotating;
+        public float RotateSpeed => rotateSpeed;
+        // Update is called once per frame
+        private void Update()
         {
-            //posMouseDown = transform.parent.localPosition;
-            //posMouseExcuted = transform.parent.localPosition;
+            RotateWernch();
         }
+
+
+        private void RotateWernch()
+        {
+            if (isRotating)
+            {
+                isMoving = true;
+                v_deltaInput = lastInput - Input.mousePosition;
+
+                Vector3 v_unitX = new Vector3(1, 0, 0);
+                Vector3 v_unitY = new Vector3(0, 1, 0);
+                projectionX = Vector3.Dot(v_unitX , v_deltaInput);
+                projectionY = Vector3.Dot(v_unitY , v_deltaInput);
+                float yInScreen = Camera.main.WorldToScreenPoint(dad.position).y;
+                float xInScreen = Camera.main.WorldToScreenPoint(dad.position).x;
+                if (Input.mousePosition.y < yInScreen)
+                    projectionX = -projectionX;
+                if (Input.mousePosition.x > xInScreen)
+                    projectionY = -projectionY;
+
+                rotateSpeed = (projectionX + projectionY) / 10f;
+                int range = 10;
+                rotateSpeed = (rotateSpeed > range) ? range : (rotateSpeed < -range) ? -range : rotateSpeed;
+                dad.Rotate(Vector3.forward, rotateSpeed);
+                lastInput = Input.mousePosition;
+            }
+            else
+            {
+                rotateSpeed = Mathf.Lerp(rotateSpeed, 0f, Time.deltaTime * 3f / stopTime);
+                dad.Rotate(Vector3.forward, rotateSpeed);
+                if(rotateSpeed == 0)
+                {
+                    isMoving = false;
+                }
+            }
+            if (Mathf.Abs(rotateSpeed) < 0.05f && !isRotating)
+            {
+                isRotating = false;
+                rotateSpeed = 0f;
+
+            }
+            lastInput = Input.mousePosition;
+        }
+
         private void OnMouseDown()
         {
-            posMouseDown = Extensions.getMouseInWorld(transform);
-            testA.position = posMouseDown;
-            isInitializing = true;
-
+            isRotating = true;
+            rotateSpeed = 0.0f;
         }
-
-        private void OnMouseDrag()
-        {
-            posMouseExcuted = Extensions.getMouseInWorld(transform);
-            testB.position = posMouseExcuted;
-            isExcuting = true;
-        }
-
-
 
         private void OnMouseUp()
         {
-            isExcuting = false;
-            isInitializing = false;
+            isRotating = false;
         }
     }
-
+    
 }
+
